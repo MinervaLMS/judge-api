5df8dc89bbfc858455f611f14edbaecd25c5b49e
Jean <jemontoyaca@unal.edu.co>
2023-08-19 18:56:56 -0500

    try

--- app/executor/executor.py
+++ app/executor/executor.py
@@ -1,2 +1,7 @@
-def print(x):
-    print(x)
+class Executor():
+    def __init__(self,output,input,code):
+        self.output = output
+        self.input = input
+        self.code = code
+    def run(self):
+        print("this is the executor")
